8d4c063cd00dd6a242308ebf16ce8ca203f49fd4
navatm <navatm@gmail.com>
2014-12-07 21:47:54 +0200

    main.html is working

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -4,16 +4,26 @@ angular
     .module('Gov', [
         // angular modules
         'ngAnimate',
-        'ngCookies',
-        'ngSanitize',
-        'ngTouch',
+        //'ngCookies',
+        //'ngSanitize',
+        //'ngTouch',
+        //'ngRoute',
 
         /// 3rd party
         'restangular',
         'ui.router',
+        //'ui.bootstrap',
 
         // features
         'Gov.Lists'
     ]).config(function(RestangularProvider, Config) {
         RestangularProvider.setBaseUrl(Config.rest.serverUrl);
-    });
+    }).config(['$stateProvider', '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise("/main");
+            $stateProvider
+            .state('main', {
+                url: "/main",
+                templateUrl: "views/main.html"
+            });
+        }]);
